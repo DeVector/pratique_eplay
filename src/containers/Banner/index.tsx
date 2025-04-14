8ff80645b969c2107bef98c481a71edbcb434f3a
DeVector <diniz.vito@gmail.com>
2025-04-14 20:25:56 -0300

    Criando os components tag e button

--- src/containers/Banner/index.tsx
+++ src/containers/Banner/index.tsx
@@ -1,19 +1,25 @@
 import * as S from './styles'
 
 import ImgBackGround from '../../images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => {
   return (
     <S.Banner style={{ backgroundImage: `url(${ImgBackGround})` }}>
       <div className="container">
-        <S.NameGame>
-          Marvel&#39;s Spider-Man: Miles Morales PS4 & PS5
-        </S.NameGame>
+        <Tag size="big">Destaque do dia</Tag>
         <div>
+          <S.NameGame>
+            Marvel&#39;s Spider-Man: Miles Morales PS4 & PS5
+          </S.NameGame>
           <S.PriceGame>
             De <span>R$ 250,00</span> <br /> por apenas R$ 99,90
           </S.PriceGame>
         </div>
+        <Button type="link" to="/produto" title="Clique aqui e aproveite agora">
+          Aproveitar
+        </Button>
       </div>
     </S.Banner>
   )
--- src/containers/Banner/styles.ts
+++ src/containers/Banner/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
+import { TagContainer } from '../Tag/styles'
 
 export const Banner = styled.div`
-  padding-top: 340px;
   padding-bottom: 64px;
   width: 100%;
   height: 560px;
@@ -9,6 +9,19 @@ export const Banner = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   font-weight: bold;
+
+  .container {
+    position: relative;
+    padding-top: 340px;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+
+  ${TagContainer} {
+    position: absolute;
+    top: 32px;
+  }
 `
 
 export const NameGame = styled.h2`
